abd967cf4b5ffa6d2dee1b2697fe4e36e162317e
HenryWauzivuff <145900606+HEofOttoman@users.noreply.github.com>
2026-01-18 23:57:26 +1100

    PA-12 - Added spring bone and jump
    - Made camera controller using springbone (buggy with close walls rn)
    - Made player controller with a lot of jump variables
    - Controller broken currently

--- resources/player/camera_controller.gd
+++ resources/player/camera_controller.gd
@@ -1,24 +1,28 @@
 extends Node3D
 
-@export var horizontal_acceleration : float = 0.2
-@export var vertical_acceleration : float = 0.2
+@export var min_limit_x : float = -0.8 ## Clamp for the lowest the camera can go
+@export var max_limit_x : float = -0.2 ## Clamp for the highest the camera can go
+@export var horizontal_acceleration : float = 2.0 ## 
+@export var vertical_acceleration : float = 2.0 ## 
+@export var mouse_acceleration : float = 0.005 ## Value to decrease the magnitude of mouse movement
 
 ## Implementation for controllers
-func _process(delta: float) -> void:
-	var joy_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	rotate_from_vector(joy_dir * delta * Vector2(horizontal_acceleration, vertical_acceleration))
+#func _process(delta: float) -> void:
+	#var joy_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	#rotate_from_vector(joy_dir * delta * Vector2(horizontal_acceleration, vertical_acceleration))
 
+## Keyboard/Mouse implementation
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		#rotation.y += event.relative.x * 0.005
-		rotate_from_vector(event.relative)
+		rotate_from_vector(event.relative * mouse_acceleration)
 
 func rotate_from_vector(event_relative_vector: Vector2):
 	if event_relative_vector.length() == 0:
 		return
-	rotation.y += event_relative_vector.x * 0.005
-	rotation.x += event_relative_vector.y * 0.0005
-	rotation.x = clamp(rotation.x, -0.1, 0.1)
+	rotation.y += event_relative_vector.x ## Left and right view rotation
+	rotation.x += event_relative_vector.y ## Up and down view rotation
+	rotation.x = clamp(rotation.x, min_limit_x, max_limit_x) ## Clamps the movement of the camera
 	
 
 ## Called when the node enters the scene tree for the first time.
--- resources/player/player_controller.gd
+++ resources/player/player_controller.gd
@@ -1,20 +1,53 @@
 extends CharacterBody3D
 
-#@export var camera : Node3D
-@onready var camera: Node3D = $CameraController/Camera3D
+@export_category("Movement")
 
+@export_group("Jump Settings")
+@export var jump_height : float = 2.25
+@export var jump_time_to_peak : float = 0.4
+@export var jump_time_to_descent : float = 0.3
 
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+
+#@export var jump_velocity : float = 4.5
+
+@export_group("Movement Settings")
 @export var base_speed : float = 4.0
 
 var movement_input : Vector2 = Vector2.ZERO
 
-func _physics_process(_delta: float) -> void:
+#@export var camera : Node3D
+@onready var camera: Node3D = $CameraController/Camera3D
+
+func _physics_process(delta: float) -> void:
 	## Up is forward, down is back
-	movement_input = Input.get_vector("move_left", "move_right", "move_up", "move_down").rotated(-camera.global_rotation.y)
+	#movement_input = Input.get_vector("move_left", "move_right", "move_up", "move_down").rotated(-camera.global_rotation.y)
 	velocity = Vector3(movement_input.x, 0, movement_input.y) * base_speed
+	
+	jump_logic(delta)
+	
+	### ui_accept is current jump action
+	#if Input.is_action_just_pressed("ui_accept"): #and is_on_floor():
+		#velocity.y = -jump_velocity
+	#var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
+	#velocity.y -= gravity * delta
 	
 	move_and_slide()
 	
+
+func jump_logic(delta) -> void:
+	if is_on_floor():
+		## ui_accept is current jump action
+		if Input.is_action_just_pressed("ui_accept"): #and is_on_floor():
+			velocity.y = -jump_velocity
+		var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
+		velocity.y -= gravity * delta
+		
+		move_and_slide()
+	else:
+		pass
 
 
 ## Template
